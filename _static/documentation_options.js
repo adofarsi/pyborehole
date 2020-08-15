4dd48263272ad2694a7bf82eeefda7be5c7c1afa
adofarsi <adofarsi@users.noreply.github.com>
2020-08-15 12:58:02 +0000

    deploy: 9293ba7da6c2ea46ec1ee3bdbb2b6913daad01fc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g2d7d0b8',
+    VERSION: '0.post1+g9293ba7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
